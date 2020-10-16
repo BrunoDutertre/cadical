cb85f0eca4fba78c630c9d09df1ad7e89b7fff95
Bruno Dutertre <bruno@csl.sri.com>
2020-10-15 18:25:05 -0700

    Fixed compiler warnings

--- src/restore.cpp
+++ src/restore.cpp
@@ -77,7 +77,7 @@ void External::restore_clauses () {
       "forced to restore all clauses");
 
   unsigned numtainted = 0;
-  for (const auto & b : tainted)
+  for (const auto b : tainted)
     if (b) numtainted++;
 
   PHASE ("restore", internal->stats.restorations,
@@ -188,7 +188,7 @@ void External::restore_clauses () {
 #endif
 
   numtainted = 0;
-  for (const auto & b : tainted)
+  for (const auto b : tainted)
     if (b) numtainted++;
 
   PHASE ("restore", internal->stats.restorations,
